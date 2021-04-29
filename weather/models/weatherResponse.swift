a45c996487c721235f48ffad8cb60702685cd2d6
p.m.student <wramirez@students.tulsatech.edu>
2021-04-29 13:19:23 -0500

    modles are done

--- weather/models/weatherResponse.swift
+++ weather/models/weatherResponse.swift
@@ -10,5 +10,9 @@ import Foundation
 struct WeatherResponse: Codable {
     var current: Weather
     var hourly: [Weather]
-//    var daily: [DailyWeather]
+    var daily: [DailyWeather]
+    
+    static func empty() -> WeatherResponse {
+        return WeatherResponse(current: Weather(), hourly: [Weather](repeating: Weather(), count: 23), daily: [DailyWeather](repeating: DailyWeather(), count: 8))
+    }
 }
